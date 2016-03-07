d8774f7f7c1d7a06f6a4fa00f18917b5d0880b30
Leo Gopal <leo@leogopal.com>
2016-03-07 11:34:55 +0200

    minor edits

--- assets/js/frontgate.js
+++ assets/js/frontgate.js
@@ -1,33 +1,36 @@
 jQuery(document).ready( function ($) {
 
 	function initialize_site_gate() {
-		$("body").addClass('overlay-active');
+		// Adds body class when overlay active to stop scrolling.
+		// $("body").addClass('overlay-active');
 
 		// Makes the overlay visible.
-		$("#nona-overlay-wrap").fadeIn(100);
+		$("#nona-overlay-wrap").fadeIn();
 
 	}
 
 	jQuery(document).bind('gform_confirmation_loaded', function(event, formId){
-        if(formId == 1) {
-            $("#nona-overlay-wrap").delay(5000).fadeOut(300);
-            $("body").removeClass('overlay-active');
-			cookie.set( 'ftds', 'verified', {
-			   expires: 180,
-			   domain: document.location.hostname,
-			   path: '/',
-			   secure: false
-			});
-        }
-    });
+			// Only work if its a specific form, formId = 1
+	    if(formId == 1) {
+					// This makes the overlay disappear with a delay and fade.
+	        // $("#nona-overlay-wrap").delay(5000).fadeOut(300);
 
-	// INIT AGE GATE
+					// This removes the body class that prevents site scrolling.
+	        // $("body").removeClass('overlay-active');
+
+					// This sets the cookie so the form is not shown again.
+					cookie.set( 'ftds', 'verified', {
+					   expires: 180,
+					   domain: document.location.hostname,
+					   path: '/',
+					   secure: false
+					});
+	    }
+	  });
+
+	// Checks if the cookie exists, displays form if it doesn't.
 	if( ! cookie.get('ftds') ) {
 		initialize_site_gate();
 	}
 
 });
-
-
-
-
